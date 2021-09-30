134ff1be86ffd035793fbdd51c446a63268d6016
syafiqajefri <syafiqajefri1705@gmail.com>
2021-09-30 10:50:56 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Kedua</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+
+    <!--contoh1
+    <?php
+    $nama = "Syafiqa";
+
+    echo 'Syafiqa $nama';
+    echo '<br>';
+    echo "Syafiqa $nama";
+
+    ?>-->
+
+    <?php
+        $nama = 'Syafiqa';
+        $umur = 18 + 2 ;
+
+        echo "$nama berumur $umur tahun ";
+
+        ?>
+</body>
+</html>
